1f7976b6c1a8490f9407c528d6f063eee65c8617
arunreddy9966 <reddyarunkumar61@gmail.com>
2024-06-04 09:05:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    < div classname="app">
+
+      <h1>hi!!! This Is My Third React Project</h1>
+      <div>
+      <img src="https://i.pinimg.com/736x/50/0c/71/500c71d6306965ba9c2a819e038c3f77.jpg"></img>
+      </div>
+      <h2>LORD SHIVA</h2>
+      
+        <div>
+        <img src="https://i.pinimg.com/736x/84/b2/53/84b2538ab34eabc94b7a4a2dea98e6d1.jpg"></img>
+      </div>
+      <h2>LORD SRIRAM</h2>
+      <div>
+        
+      </div>
+      
+      </div>
+  );
+}
+
+export default App;
